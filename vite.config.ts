75974ba6bedded0981d447f583b2a845d79cef38
John-uz <gzfeng.john@gmail.com>
2025-12-16 04:37:43 +0500

    rerefix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GitHub Pages
+  base: './', // Ensures assets are loaded correctly on GitHub Pages relative paths
   build: {
     outDir: 'dist',
   }
